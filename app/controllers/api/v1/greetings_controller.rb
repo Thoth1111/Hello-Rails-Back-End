6a60f27b7a77e80817483c4606689430b184cf37
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-06-14 12:29:56 +0300

    Create functional api endpoint

---
+++ app/controllers/api/v1/greetings_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::GreetingsController < ApplicationController
+
+  def show
+    greeting = Greeting.order('RANDOM()').first
+    render json: { message: greeting.message }
+  end
+
+end
